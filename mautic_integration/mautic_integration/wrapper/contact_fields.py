fd3e964596eef58a06d2fd70eb5985e9263b2c4f
Charles-Henri Decultot <chdecultot@dokos.io>
2018-06-27 14:25:29 +0000

    Old files removed

--- mautic_integration/mautic_integration/wrapper/contact_fields.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Initial code by https://github.com/divio/python-mautic
-from __future__ import unicode_literals, absolute_import
-
-from .api import API
-
-
-class ContactFields(API):
-    _endpoint = 'fields/contact'
