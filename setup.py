632897830f2b022e8a9ab880819318c98b3ca3eb
Charles-Henri Decultot <chdecultot@dokos.io>
2018-06-30 08:31:59 +0000

    Setup correction

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ with open('mautic/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
-
 setup(
 	name='mautic',
 	version=version,
